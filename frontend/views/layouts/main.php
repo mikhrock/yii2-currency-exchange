067e9a3d296830177c527a99eec579e02235d226
Mikhail Kolker <mikhail.kolker@gmail.com>
2019-04-14 21:46:29 +0000

    menu fix

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -38,7 +38,7 @@ AppAsset::register($this);
     $menuItems = [
         ['label' => 'Home', 'url' => ['/site/index']],
         ['label' => 'Wallets', 'url' => ['/wallet']],
-        ['label' => 'Currency Exchange', 'url' => ['/currencyrate']],
+        ['label' => 'Currency Rate', 'url' => ['/currencyrate']],
     ];
     if (Yii::$app->user->isGuest) {
         $menuItems[] = ['label' => 'Signup', 'url' => ['/site/signup']];
